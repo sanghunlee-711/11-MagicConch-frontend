8b2bacd13318286e98313e010a03d1acee5ed013
cloudlee <cloudlee@MacBook-Pro-9.local>
2020-09-09 15:19:24 +0900

    Refactor: 충돌사항 및 버튼컴포넌트 생성

---
+++ src/Components/Button/GoPrevButton.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function GoPrevButton() {
+  return (
+    <svg viewBox="0 0 8px 8px" role="presentation" class="icon--chevron-left">
+      <svg id="chevron-left" viewBox="0 0 8.3px 14.1px">
+        <polygon
+          class="st0"
+          points="7,0 8.3,1.3 2.5,7.1 8.3,12.8 7,14.1 0,7.1 "
+        ></polygon>
+      </svg>
+    </svg>
+  );
+}
